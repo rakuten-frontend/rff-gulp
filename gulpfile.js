c193050cc505581b43dc1cce8441ca4cbdf4e078
Hiroyuki Tanjo <expheno@gmail.com>
2016-02-23 09:47:40 +0900

    Add clean task

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@
 
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
+var runSequence = require('run-sequence');
+var del = require('del');
 
 gulp.task('zip', function () {
   return gulp.src([
@@ -12,4 +14,10 @@ gulp.task('zip', function () {
   .pipe($.ignore.include({isFile: true}))
     .pipe($.zip('archive.zip'))
     .pipe(gulp.dest('dist'));
+});
+
+gulp.task('clean', del.bind(null, 'dist'));
+
+gulp.task('default', function (callback) {
+  runSequence('clean', 'zip', callback);
 });
